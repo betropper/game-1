7fdd680f68d944ffbcd49bf0638235f83e194ee4
Benjamin Muhlestein <betropper@gmail.com>
2015-09-22 17:55:24 -0400

    save

--- flappybird/game.js
+++ flappybird/game.js
@@ -4,15 +4,50 @@ var background;
 function preload() {
   game.stage.backgroundColor = '#bbbbbb';
   game.load.image("background","assets/bg.png");
+  game.load.image("bird", "assets/bird.png");
+//  game.load.image("pipe," "assets/pipe.png");
 }
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
   background = game.add.tileSprite(0,0,320,568,"background");
   background.autoScroll(-100,0);
+  bird = game.add.sprite(100, 245, 'bird');
+  game.physics.arcade.enable(bird);
+  bird.body.gravity.y = 1000;
+//  pipes = game.add.group(); // Create a group  
+//  game.physics.arcade.enable(pipes);  // Add physics to the group  
+//  pipes.createMultiple(4, 'pipe'); // Create 20 pipes  
+  var spaceKey =
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(jump);
 }
 
 function update() {
+  if (bird.inWorld == false)
+    restart.game();
 
 }
+
+function jump() {
+  bird.body.velocity.y = -350;
+}
+
+// function addOnePipe() {
+//  var pipe = pipes.getFirstDead();
+//  pipe.reset(x, y);
+//  pipe.body.velocity.x = -200;
+//  pipe.checkWorldBounds = true;
+//  pipe.outOfBoundsKill = true;
+// }
+
+//function addTwoPipes() {
+  
+//}
+
+//function restartGame() {
+//  game.state.start('main');
+//}
+
 
 function render() {
 
